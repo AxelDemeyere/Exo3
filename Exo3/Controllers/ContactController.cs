e01795a795e3c66d4006fc465fb916e47caf1062
Axel Demeyere <axel.demeyere59@gmail.com>
2026-01-06 15:12:00 +0100

    Initial commit

---
+++ Exo3/Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+﻿using Exo3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exo3.Controllers;
+
+public class ContactController : Controller
+{
+    private readonly ILogger<ContactController> _logger;
+
+    public ContactController(ILogger<ContactController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Contacts()
+    {
+        var contacts = ContactViewModel.GetAllContacts();
+        return View(contacts);
+    }
+
+    public IActionResult Contact(int id)
+    {
+        var contacts = ContactViewModel.GetAllContacts();
+        var contact = contacts.FirstOrDefault(c => c.Id == id);
+        
+        if (contact == null)
+        {
+            return NotFound();
+        }
+        
+        return View(contact);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(ContactViewModel contact)
+    {
+        if (!ModelState.IsValid) return View(contact);
+        
+        var contacts = ContactViewModel.GetAllContacts();
+        contact.Id = contacts.Any() ? contacts.Max(c => c.Id) + 1 : 1;
+        
+        contacts.Add(contact);
+        return RedirectToAction("Contacts");
+    }
+}
---
+++ Exo3/Models/ContactViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exo3.Models;
+
+public class ContactViewModel
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Le nom est requis")]
+    [Display(Name = "Nom")]
+    public string Name { get; set; } = "";
+    
+    [Display(Name = "E-mail")]
+    [Required(ErrorMessage = "L'e-mail est requis")]
+    [EmailAddress] 
+    public string Email { get; set; } = "";
+    
+    private static List<ContactViewModel> _contacts = new List<ContactViewModel>
+    {
+        new ContactViewModel(1, "Alice Smith", "alice.smith@gmail.com"),
+        new ContactViewModel(2, "Bob Johnson", "bob.johnson@gmail.com"),
+        new ContactViewModel(3, "Charlie Brown", "charlie.brown@gmail.com"),
+    };
+    
+    public ContactViewModel() {}
+    public ContactViewModel(int id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
+
+    public static List<ContactViewModel> GetAllContacts()
+    {
+        return _contacts;
+    }
+}
